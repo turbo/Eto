cc3337a3a0e8530431fd40405a7d7dc776d857c3
turbo <minxomat@gmail.com>
2018-09-19 13:26:26 +0200

    GTK Test as Mono instead of core

---
+++ src/GtkMono/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Eto.Test;
+
+namespace GtkMono
+{
+	internal class Startup
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			var generator = new Eto.GtkSharp.Platform();
+			
+			var app = new TestApplication(generator);
+			app.TestAssemblies.Add(typeof(Startup).Assembly);
+			app.Run();
+		}
+	}
+}
